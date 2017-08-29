248eeaac1d8d9fc93ac5ad80f505b2faa76124ff
Leonardo Abaque <leoabaque@hotmail.com>
2017-08-29 15:00:11 -0300

    Adicao do codigo fonte.

---
+++ main.c
@@ -0,0 +1,281 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+#define LINHAS 11
+#define COLUNAS 11
+
+char mapFrontPlayer[LINHAS][COLUNAS], mapFrontBot[LINHAS][COLUNAS];
+char mapBackPlayer[LINHAS][COLUNAS], mapBackBot[LINHAS][COLUNAS];
+
+int maxBarcos = 5, playerHit, botHit, playerShot, botShot, i = 0;
+int tentativasBot[100];
+
+void PrintMap(char mapa[LINHAS][COLUNAS])
+{
+    for(int i = 0; i < LINHAS; i++)
+    {
+        for(int j = 0; j < COLUNAS; j++)
+        {
+            printf("%c ", mapa[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void Iniciar()
+{
+    for(int i=0; i < LINHAS; i++)
+    {
+        for(int j=0; j < COLUNAS; j++)
+        {
+            if(i > 0 && j > 0)
+            {
+                mapFrontPlayer[i][j] = '~';
+                mapBackPlayer[i][j] = '~';
+                mapFrontBot[i][j] = '~';
+                mapBackBot[i][j] = '~';
+            }
+            else if(i > 0 && j == 0)
+            {
+                char lin = 64;
+                lin += i;
+                mapFrontPlayer[i][j] = lin;
+                mapBackPlayer[i][j] = lin;
+                mapFrontBot[i][j] = lin;
+                mapBackBot[i][j] = lin;
+            }
+            else if(i == 0 && j > 0)
+            {
+                sprintf(&mapFrontPlayer[i][j], "%d", j);
+                sprintf(&mapBackPlayer[i][j], "%d", j);
+                sprintf(&mapFrontBot[i][j], "%d", j);
+                sprintf(&mapBackBot[i][j], "%d", j);
+            }
+        }
+    }
+}
+
+void SetPlayerShips(char mapFront[LINHAS][COLUNAS], char mapBack[LINHAS][COLUNAS])
+{
+    srand(time(NULL));
+    int contador = 0;
+    while(contador < maxBarcos)
+    {
+        int x = rand() % LINHAS;
+        if(x == 0)
+            x = 1;
+
+        int y = rand() % COLUNAS;
+        if(y == 0)
+            y = 1;
+
+        if(mapBack[x][y] != 'b')
+        {
+            contador++;
+            mapFront[x][y] = 'b';
+            mapBack[x][y] = 'b';
+        }
+    }
+}
+
+void SetBotShips(char mapBack[LINHAS][COLUNAS])
+{
+    srand(time(NULL));
+    int contador = 0;
+    while(contador < maxBarcos)
+    {
+        int x = rand() % LINHAS;
+        if(x == 0)
+            x = 1;
+
+        int y = rand() % COLUNAS;
+        if(y == 0)
+            y = 1;
+
+        if(mapBack[x][y] != 'b')
+        {
+            contador++;
+            mapBack[x][y] = 'b';
+        }
+    }
+}
+
+int AtirarPlayer()
+{
+    char xS;
+    int y, x;
+    printf("\nEscolha onde atirar com as coordenadas (o ultimo numero '1' equivale a 10 (dez)): ");
+    scanf("%s %d", &xS, &y);
+
+    if(xS == 'a' || xS == 'A')
+        x = 1;
+    else if(xS == 'b' || xS == 'B')
+        x = 2;
+    else if(xS == 'c' || xS == 'C')
+        x = 3;
+    else if(xS == 'd' || xS == 'D')
+        x = 4;
+    else if(xS == 'e' || xS == 'E')
+        x = 5;
+    else if(xS == 'f' || xS == 'F')
+        x = 6;
+    else if(xS == 'g' || xS == 'G')
+        x = 7;
+    else if(xS == 'h' || xS == 'H')
+        x = 8;
+    else if(xS == 'i' || xS == 'I')
+        x = 9;
+    else if(xS == 'j' || xS == 'J')
+        x = 10;
+    else
+        return 4;
+
+    if(mapBackBot[x][y] == 'b')
+    {
+        mapBackBot[x][y] = 'x';
+        mapFrontBot[x][y] = 'x';
+
+        return 1;
+    }
+    else if (mapBackBot[x][y] == '~')
+    {
+        mapBackBot[x][y] = 'o';
+        mapFrontBot[x][y] = 'o';
+
+        return 2;
+    }
+    else
+        return 3;
+}
+
+int AtirarBot()
+{
+    srand(time(NULL));
+    int x = rand() % LINHAS;
+    int y = rand() % COLUNAS;
+
+    for(int saveI = 0; saveI <= i; saveI++)
+    {
+        if(tentativasBot[saveI] == mapBackPlayer[x][y])
+        {
+            x = rand() % LINHAS;
+            y = rand() % COLUNAS;
+        }
+    }
+
+    if(mapBackPlayer[x][y] == 'b')
+    {
+        mapBackPlayer[x][y] = 'x';
+        mapFrontPlayer[x][y] = 'x';
+        tentativasBot[i] = mapBackPlayer[x][y];
+        i++;
+
+        return 1;
+    }
+    else if (mapBackPlayer[x][y] == '~')
+    {
+        mapBackPlayer[x][y] = 'o';
+        mapFrontPlayer[x][y] = 'o';
+        tentativasBot[i] = mapBackPlayer[x][y];
+        i++;
+
+        return 2;
+    }
+}
+
+void ClearScreen()
+{
+    system("cls");
+}
+
+void Maps()
+{
+    printf("Suas Embarcacoes: ");
+    printf("Afundadas: %d/%d \n\n", botHit, maxBarcos);
+    PrintMap(mapFrontPlayer);
+    printf("\nSeus Tiros: ");
+    printf("Quantidade de acertos: %d/%d \n\n", playerHit, maxBarcos);
+    PrintMap(mapFrontBot);
+}
+
+int main()
+{
+    Iniciar();
+
+    SetPlayerShips(mapFrontPlayer, mapBackPlayer);
+    SetBotShips(mapBackBot);
+
+    while(playerHit < 5 || botHit < 5)
+    {
+        Maps();
+
+        playerShot = AtirarPlayer();
+
+        ClearScreen();
+
+        if(playerShot == 1)
+        {
+            printf("======================================\n");
+            printf("Parabens, voce afundou uma embarcacao!\n");
+            printf("======================================\n");
+            printf("\n\nPressione qualquer tecla para continuar...");
+            playerHit += playerShot;
+            getch();
+        }
+        else if(playerShot == 2)
+        {
+            printf("======================================\n");
+            printf("Tiro na agua!\n");
+            printf("======================================\n");
+            printf("\n\nPressione qualquer tecla para continuar...");
+            getch();
+        }
+        else
+        {
+            printf("======================================\n");
+            printf("Coordenada Invalida!\n");
+            printf("======================================\n");
+            printf("\n\nPressione qualquer tecla para continuar...");
+            getch();
+        }
+
+        ClearScreen();
+
+        printf("======================================\n");
+        printf("Eh a vez de seu oponente!\n");
+        printf("======================================\n");
+        printf("\n\nPressione qualquer tecla para continuar...");
+        getch();
+
+        ClearScreen();
+
+        botShot = AtirarBot();
+
+        if(botShot == 1)
+        {
+            printf("======================================\n");
+            printf("Bot says: I hit one of your boats!\n");
+            printf("======================================\n");
+            printf("\n\nPressione qualquer tecla para continuar...");
+            botHit += botShot;
+            getch();
+        }
+        else if(botShot == 2)
+        {
+            printf("======================================\n");
+            printf("Bot says: I missed my shot!\n");
+            printf("======================================\n");
+            printf("\n\nPressione qualquer tecla para continuar...");
+            getch();
+        }
+
+        ClearScreen();
+    }
+    if(botHit == 5)
+        printf("Que pena, voce perdeu!");
+    else
+        printf("Parabens, voce afundou todas as embarcacoes inimigas!");
+    return 0;
+}
